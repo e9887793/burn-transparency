f8c639795a41a1e1e8862740e0a029a96c884077
Ethan Wood <ewood@Ethans-MacBook-Pro.local>
2024-09-12 13:22:36 -0500

    updating

--- script.R
+++ script.R
@@ -6,23 +6,25 @@ devtools::install_github("quest-bih/oddpub")
 devtools::install_github("cran/crminer")
 devtools::install_github("serghiou/metareadr")
 devtools::install_github("serghiou/rtransparent", build_vignettes = T)
+library(dplyr)
+library(rtransparent)
 
 # the vignettes let you get an idea for what rtransparency does
 # if you download the example article and run the commands, the package will pull transparency-related information out
 # of the example article
 
 #Example usage
-library(rtransparent)
 #only required if pdftotext shows up as missing
 Sys.setenv(PATH = paste(old_path, "/usr/local/bin/", sep = ":"))
-article <- rt_read_pdf("/Users/ewood/Downloads/PMID323740764.pdf")
+article <- rt_read_pdf("data/PMID323740764.pdf")
 cat(substr(article, start = 1, stop = 200))
 write(article, "PMID323740764.txt")
 data_code <- rt_data_code("PMID323740764.txt")
-library(dplyr)
 glimpse(data_code)
 all_indicators <- rt_all("PMID323740764.txt")
 glimpse(all_indicators)
+data_code <- rt_data_code("PMID323740764.txt")
+glimpse(data_code)
 
 #Convert PDF files to txt
 files <- list.files(path="/Users/ewood/transparency/data", pattern="*.pdf", full.names=TRUE, recursive=FALSE)
